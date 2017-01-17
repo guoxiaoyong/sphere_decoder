10a4200cce822eb450009a98980600b143ab45d2
Xiaoyong Guo <guo.xiaoyong@gmail.com>
2017-01-17 08:43:12 +0800

    add LICENSE and other stuff

---
+++ example.m
@@ -0,0 +1,32 @@
+\documentclass{article}
+\title{How to use \texttt{sphdec.m}}
+\author{Xiaoyong Guo}
+
+\begin{document}
+\maketitle
+
+Alamouti code:
+\begin{equation}
+X(c_1, c_2) = 
+\begin{bmatrix}
+c_1 & c_2 \\
+-c_2^* c_1^*
+\end{bmatrix}
+
+\begin{equation}
+H =
+\begin{bmatrix}
+h_1\\
+h_2
+\end{bmatrix}
+\end{equation}
+
+\begin{equation}
+Y = Xh + w
+\end{equation}
+where $w$ is a random noise vector, 
+with distribution $N(0, SNR)$.
+\end{equation}
+
+\end{document}
+
---
+++ golden_code.m
@@ -0,0 +1,8 @@
+function gen_equivalent_channel_matrix()
+theta = (1 + sqrt(5))/2;
+alpha = 1 + j - j*theta;
+sigma_theta = 1 - theta;
+s1 = alpha;
+s2 = alpha * theta;
+s3 = alpha;
+s4 = 
